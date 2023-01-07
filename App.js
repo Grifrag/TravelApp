b3dddecf42b8680162007a24ed2744e49140f93f
e18173 <e18173@unipi.gr>
2023-01-07 06:22:34 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from 'react';
+import Home from './Home';
+
+
+const App = () => {
+  return (
+    <Home />
+  );
+};
+
+
+export default App;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, View } from 'react-native';
+import Title from './title';
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Title />
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default Home;
