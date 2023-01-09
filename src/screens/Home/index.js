c7de0815f029fcd42fea7d0f3f954924ee1badf4
e18173 <e18173@unipi.gr>
2023-01-09 17:29:53 +0200

    home drie

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SafeAreaView, View } from 'react-native';
-import Title from './title';
+import Title from '../../components/Title';
 
 const Home = () => {
     return (
