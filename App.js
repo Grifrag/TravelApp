879f109ebd248a0ada07216f6a922db746720e32
grifrag <grifrag@gmail.com>
2023-07-09 03:54:50 +0300

    Revert Changes

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
 import Home from './src/screens/Home';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  background-color: white;
-`;
 
 
 const App = () => {
   return (
-    <Container>
-      <Home />
-    </Container>
+    <Home />
   );
 };
 
