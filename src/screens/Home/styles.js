0e1b759095634e5c3cd4970717d2de86a772a8f8
e18173 <e18173@unipi.gr>
2023-07-09 04:01:13 +0300

    k

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         margin: 32,
+        backgroundColor: '#FFFFFF',
     },
     subtitle: {
         fontSize: 20,
